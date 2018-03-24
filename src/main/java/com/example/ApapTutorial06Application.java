ac3358f2837939fc0920850232dc6f1ab905320a
M Kamal <muh.kmal@gmail.com>
2018-03-24 21:59:29 +0700

    commit tutorial06

---
+++ src/main/java/com/example/ApapTutorial06Application.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApapTutorial06Application
+{
+
+    public static void main (String[] args)
+    {
+        SpringApplication.run (ApapTutorial06Application.class, args);
+    }
+}
